391098838eb312f434bf82eee31c144d389ef956
Luke Carrier <luke@carrier.family>
2026-03-03 23:36:37 +0000

    feat(env): secret masking

--- pkg/runner/expression.go
+++ pkg/runner/expression.go
@@ -601,6 +601,10 @@ func getWorkflowSecrets(ctx context.Context, rc *RunContext) map[string]string {
 			if envConfig != nil {
 				for k, v := range envConfig.Secrets {
 					secrets[k] = v
+					// Add environment-specific secrets to masks
+					if !rc.Config.InsecureSecrets {
+						rc.AddMask(v)
+					}
 				}
 			}
 		}
